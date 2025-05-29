e69db0ddcc020b6fd9637c89e0e0da444f7e00a9
mouleen <limauropro@gmail.com>
2025-05-29 08:27:38 +0000

    Mejoras funcionales y estaticas de Favoritos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
-
 export const Navbar = () => {
-	const {store} =useGlobalReducer()
+	const {store,dispatch} =useGlobalReducer()
+	
+	const handleDeleteFavorites=(type,id)=>{
+		let fav=[...store.favorites]
+		let favFiltered=fav.filter(persona => (persona.type == type && persona.id == id))
+		if(favFiltered.length > 0){
+			let favFilterRemove=fav.filter(persona => (!(persona.type == type && persona.id == id)))
+			dispatch({type: 'get_favorites',payload: favFilterRemove });    
+		}else{
+			alert('no existe el elemento que intenta eliminar')
+		}
+	}
+	
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
@@ -19,12 +30,13 @@ export const Navbar = () => {
 							
 							{
 								store.favorites.map((item,idx)=>(
-								<li key={idx}><button className="dropdown-item" type="button">{item.name}</button></li>
+								<li key={idx} style={{width:'300px'}}>
+									<button className="dropdown-item" style={{display: "inline-block",width:"90%"}} type="button">{item.name}</button>
+									<i class="fa-regular fa-trash-can" style={{display: "inline-block",width:"10%"}} onClick={()=>{handleDeleteFavorites(item.type,item.id)}}></i>
+								</li>
 								))
 						}
-						{/*<li><button className="dropdown-item" type="button">Dropdown item</button></li>
-						<li><button className="dropdown-item" type="button">Dropdown item</button></li>
-						<li><button className="dropdown-item" type="button">Dropdown item</button></li>*/}
+						
 						</ul>
 					</div>
 				</div>
